117bdff6b1e07db84f3c16b5cede44549838ee66
Cedrick Dubois <c.dubois@axiofleet.com>
2025-08-17 21:03:24 +0200

    ui: désactive StickyCTA pour supprimer toute scroll-bar bas de page

--- src/components/common/StickyCTA.jsx
+++ src/components/common/StickyCTA.jsx
@@ -1,37 +1,6 @@
-﻿import * as React from "react";
-import { Button, Slide, Paper } from "@mui/material";
-import { Link, useLocation } from "react-router-dom";
-import { track } from "../../lib/analytics/plausible";
+﻿// Désactivé : plus aucun sticky CTA en bas de page (mobile ou autre)
+import * as React from "react";
 
 export default function StickyCTA() {
-  const { pathname } = useLocation();
-  const show = pathname !== "/contact";
-  return (
-    <Slide direction="up" in={show} mountOnEnter unmountOnExit>
-      <Paper
-        elevation={6}
-        sx={{
-          position: "fixed",
-          left: 0,
-          right: 0,
-          bottom: 0,
-          p: 1,
-          display: { xs: "flex", md: "none" },
-          justifyContent: "center",
-          bgcolor: "background.paper",
-          borderTop: "1px solid rgba(255,255,255,.06)",
-        }}
-      >
-        <Button
-          component={Link}
-          to="/contact"
-          size="large"
-          variant="contained"
-          onClick={() => track("CTA_Devis", { source: "sticky_mobile" })}
-        >
-          Demander un devis
-        </Button>
-      </Paper>
-    </Slide>
-  );
+  return null;
 }
